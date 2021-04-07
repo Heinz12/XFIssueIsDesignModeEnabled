a09e4f0c4e0b6422dca75ab9161072ab9ef330e4
Heinz Holzer <holzerheinz@hotmail.com>
2021-04-07 11:56:13 +0200

    Add project files.

---
+++ XFIssueIsDesignModeEnabled/XFIssueIsDesignModeEnabled/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XFIssueIsDesignModeEnabled.Services;
+using XFIssueIsDesignModeEnabled.Views;
+
+namespace XFIssueIsDesignModeEnabled
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+            if (DesignMode.IsDesignModeEnabled) 
+                return;
+
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ XFIssueIsDesignModeEnabled/XFIssueIsDesignModeEnabled/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XFIssueIsDesignModeEnabled.ViewModels;
+
+namespace XFIssueIsDesignModeEnabled.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
